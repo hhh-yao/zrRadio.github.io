adb149e0267ebedbe1c8785eeb8d82e3ca22ceda
hhh-yao <2863475253@qq.com>
2021-07-24 18:40:31 +0800

    Site updated: 2021-07-24 18:40:22

--- flarum/storage/views/996be352de55cb0becd24aa2002298ded926d38f.php
+++
@@ -1,8 +0,0 @@
-<?php $__env->startSection('content'); ?>
-  <p>
-    <?php echo e($message); ?>
-
-  </p>
-<?php $__env->stopSection(); ?>
-
-<?php echo $__env->make('flarum.forum::layouts.basic', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH F:\httpd-2.4.48-win64-VS16\Apache24\htdocs\flarum\vendor\flarum\core\src\Forum/../../views/error/default.blade.php ENDPATH**/ ?>
